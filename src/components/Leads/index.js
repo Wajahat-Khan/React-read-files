51cb5711f973111309db894bbe328cacf361fe65
Wajahat Hussain <wkhan.bese15seecs@seecs.edu.pk>
2021-09-13 02:16:06 +0500

    login page setup done

---
+++ src/components/Leads/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+class LeadsPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(<div>Leads</div>)
+  }
+}
+
+
+export default LeadsPage;
---
+++ src/components/Overview/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+class OverviewPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(<div>Overview</div>)
+  }
+}
+
+
+export default OverviewPage;
